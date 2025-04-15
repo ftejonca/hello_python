a47ca449f1814a31cc40c38fa69db2109a489992
ftejonca <ftejoncadev@gmail.com>
2025-04-15 16:33:40 +0200

    Se agrega el archivo de los operadores

---
+++ 02_operators.py
@@ -0,0 +1,34 @@
+### Operadores aritméticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3) # floor division, siempre intenta aproximarlo a un número entero aunque de un decimal
+print(2 ** 3) # elevar al exponente, es decir, 2 elevado a 3
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola " + "Python" + "! " + "Qué tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores comparativos ###
+
+print(3 > 4) # mayor que
+print(3 < 4) # menor que
+print(3 >= 4) # mayor o igual que
+print(3 <= 4) # menor o igual que
+print(3 == 4) # igual que
+print(3 != 4) # distinto que
+
+print("Hola" > "Python") 
+print("Hola" < "Python") 
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
