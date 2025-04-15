7e72d771d41fdc929aeb8856d2a05b321642e0e3
ftejonca <ftejoncadev@gmail.com>
2025-04-15 13:17:25 +0200

    primer commit para probar

---
+++ hello.py
@@ -0,0 +1,20 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un comentario
+en varias
+líneas
+"""
+
+'''
+Este es un comentario
+en varias
+líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola Python")) #Tipo 'str'
+print(type(5)) #Tipo 'int'
