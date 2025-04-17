ea63a7da9bc0035a8a61dd7c9173acc8c0a264e5
ftejonca <ftejoncadev@gmail.com>
2025-04-18 00:21:55 +0200

    Se modifica el archivo 06 y se añaden el 07 y el 08

---
+++ 08_conditionals.py
@@ -0,0 +1,20 @@
+### Condicionales ###
+
+my_condition = False
+
+if my_condition: # Es lo mismo que if my_condition == True:
+    print("Se ejecuta la condición del if")
+    
+my_condition = 1 * 1
+    
+if my_condition == 10:
+    print("Se ejecuta la condición del segundo if")
+    
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 1:
+    print("Es igual a 1")
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20")
+
+print("La ejecución continúa")
