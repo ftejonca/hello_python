1ffe43afbe66c0334c4da5311f76c7e645c08b7e
DevNando <nandotc7@gmail.com>
2025-04-20 00:37:19 +0200

    Se comienza la parte intermedia, con el archivo 00 sobre fechas

--- Intermediate/00_dates.py
+++ Intermediate/00_dates.py
@@ -1,1 +1,41 @@
 ### Fechas ###
+
+from datetime import datetime
+from datetime import time
+from datetime import date
+
+now = datetime.now()
+
+def print_date(date):
+    print(date.day)
+    print(date.month)
+    print(date.year)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
+    
+print_date(now)
+
+year_2025 = datetime(2025,4, 20)
+
+print_date(year_2025)
+
+current_time = time(00, 30, 21)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+current_date = date.today()
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(2025, 4, 20)
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
