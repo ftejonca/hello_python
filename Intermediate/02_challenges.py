dfcca6e21483af61e9373d4ddcc6c24a3152e7b6
DevNando <nandotc7@gmail.com>
2025-04-21 23:43:44 +0200

    Se continúa con los retos, se termina fibonacci

--- Intermediate/02_challenges.py
+++ Intermediate/02_challenges.py
@@ -70,4 +70,26 @@ def anagrama(word_one, word_two):
         return False
     return sorted(word_one.lower()) == sorted(word_two.lower())
         
-print(anagrama("roma", "amor"))
+#print(anagrama("roma", "amor"))
+
+"""
+LA SUCESIÓN DE FIBONACCI
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+la que el siguiente siempre es la suma de los dos anteriores.
+0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def fibonacci():
+    prev = 0
+    next = 1
+
+    for index in range(0, 50):
+        print(prev)
+        fib = prev + next
+        prev = next
+        next = fib
+        
+        
+fibonacci()
